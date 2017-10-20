a97d01887008ff7af8e4e825802e2cef3bb6972b
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-10-20 23:16:09 +0200

    redirect to /izseki on broken snippet

--- src/components/pages/Snippet.vue
+++ src/components/pages/Snippet.vue
@@ -76,6 +76,8 @@ export default {
       console.log(snippetSuccess.data.id);
       if (snippetSuccess.data.id === 0) {
         this.$router.push({ name: 'Collections' });
+      } else if (snippetSuccess.data.video_id === '4') {
+        this.$router.push({ name: 'Collections' });
       } else {
         // set video to loop and set start and end times
         this.$store.commit('video/UPDATE_LOOP_START', (snippetSuccess.data.start_time / 1000));
